7d123f20ccde23a08f1eebca6e977873eea30dfa
AguuSz <agus.sepu2000@hotmail.com>
2019-04-08 08:49:03 -0300

    Conversion de una imagen a un array para entrenamiento

--- faces-train.py
+++ faces-train.py
@@ -1,10 +1,19 @@
 import os
+import numpy as np
+from PIL import Image
 
-BASE_DIR = os.path.dirnmae(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(BASE_DIR, "images")
+
+x_train = []
+y_train = []
 
 for root, dirs, files in os.walk(image_dir):
     for file in files:
         if file.endswith("png") or file.endswith("jpg"):
             path = os.path.join(root, file)
-            print(path)
+            label = os.path.basename(root).replace(" ", "-").lower()
+            print(label, path)
+
+            pil_image = Image.open(path).convert("L")  # Convierte a gris
+            image_array = np.array(pil_image, "uint8") # Convierte a un array de numeros la img gris
