d422f8eaf1ce93b86016fcf6231929b85e1b56ac
AguuSz <agus.sepu2000@hotmail.com>
2019-04-08 09:26:42 -0300

    Avances faces.py

--- faces.py
+++ faces.py
@@ -2,6 +2,8 @@ import numpy as np
 import cv2
 
 face_cascade = cv2.CascadeClassifier("data/haarcascade_frontalface_alt2.xml")
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainner.yml")
 
 cap = cv2.VideoCapture(0)
 
@@ -13,6 +15,12 @@ while (True):
     for (x, y, w, h) in faces:
         roi_gray = gray[y:y + h, x:x + w]
         roi_color = frame[y:y + h, x:x + w]
+
+        # Para reconocer necesitamos un modelo de prediccion /keras /tensorflor /pytorch / sickit-learn
+        id_, conf = recognizer.predict(roi_gray)
+        if conf >= 45 and conf <= 85:
+            print(id_)
+
         img_item = "my-image.png"
         cv2.imwrite(img_item, roi_gray)
 
