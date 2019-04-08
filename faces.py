3977f89de71240c14d3fb85e7c17cc0803f26985
AguuSz <agus.sepu2000@hotmail.com>
2019-04-08 09:48:34 -0300

    faces.py funcionando pero incorrectamente

--- faces.py
+++ faces.py
@@ -1,9 +1,15 @@
 import numpy as np
 import cv2
+import pickle
 
 face_cascade = cv2.CascadeClassifier("data/haarcascade_frontalface_alt2.xml")
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read("trainner.yml")
+
+labels = {}
+with open("labels.pickle", "rb") as f:
+    og_labels = pickle.load(f)
+    labels = {v: k for k, v in og_labels.items()}
 
 cap = cv2.VideoCapture(0)
 
@@ -18,8 +24,14 @@ while (True):
 
         # Para reconocer necesitamos un modelo de prediccion /keras /tensorflor /pytorch / sickit-learn
         id_, conf = recognizer.predict(roi_gray)
-        if conf >= 45 and conf <= 85:
+        if conf >= 45:  # and conf <= 85:
             print(id_)
+            print(labels[id_])
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            name = labels[id_]
+            color = (255, 255, 255)
+            stroke = 2
+            cv2.putText(frame, name, (x, y), font, 1, color, stroke, cv2.LINE_AA)
 
         img_item = "my-image.png"
         cv2.imwrite(img_item, roi_gray)
