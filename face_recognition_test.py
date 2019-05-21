87bbcf32e63c953255735ad99ea0eb0700902ade
AguuSz <agus.sepu2000@hotmail.com>
2019-05-21 09:09:51 -0300

    Reorganizacion de archivos

--- face_recognition_test.py
+++ face_recognition_test.py
@@ -6,46 +6,41 @@ import numpy as np
 video_capture = cv2.VideoCapture(0)
 
 # Cargar una imagen de prueba y aprende a reconocerla
-agus = face_recognition.load_image_file('./imagenes/entrenamiento/agus2.jpeg')
+agus = face_recognition.load_image_file('./data_img/agus.jpeg')
 agus_encoding = face_recognition.face_encodings(agus)[0]
 
 # Cargue otra imagen de entrenamiento y que aprenda a reconocerla
-ana = face_recognition.load_image_file('./imagenes/entrenamiento/ana.jpeg')
-ana_encoding = face_recognition.face_encodings(ana)[0]
-
-# Cargue otra imagen de entrenamiento y que aprenda a reconocerla
-johana = face_recognition.load_image_file('./imagenes/entrenamiento/yohana.jpeg')
-johana_encoding = face_recognition.face_encodings(johana)[0]
-
-# Cargue otra imagen de entrenamiento y que aprenda a reconocerla
-gordo = face_recognition.load_image_file('./imagenes/entrenamiento/gordo.jpeg')
-gordo_encoding = face_recognition.face_encodings(gordo)[0]
-
-# Cargue otra imagen de entrenamiento y que aprenda a reconocerla
-chad = face_recognition.load_image_file('./imagenes/entrenamiento/chad.jpeg')
+chad = face_recognition.load_image_file('./data_img/chad.jpeg')
 chad_encoding = face_recognition.face_encodings(chad)[0]
 
 # Cargue otra imagen de entrenamiento y que aprenda a reconocerla
-will = face_recognition.load_image_file('./imagenes/entrenamiento/will.jpeg')
+will = face_recognition.load_image_file('./data_img/will.jpeg')
 will_encoding = face_recognition.face_encodings(will)[0]
+
+# Cargue otra imagen de entrenamiento y que aprenda a reconocerla
+marcos = face_recognition.load_image_file('./data_img/marcos.jpeg')
+marcos_encoding = face_recognition.face_encodings(marcos)[0]
+
+# Cargue otra imagen de entrenamiento y que aprenda a reconocerla
+valen = face_recognition.load_image_file('./data_img/valen.jpeg')
+valen_encoding = face_recognition.face_encodings(valen)[0]
+
 
 # Crear una array con los encodings de las caras y sus nombres
 known_face_encodings = [
     agus_encoding,
-    ana_encoding,
-    johana_encoding,
-    gordo_encoding,
     chad_encoding,
-    will_encoding
+    will_encoding,
+    marcos_encoding,
+    valen_encoding
 
 ]
 known_face_names = [
-    "Agustin",
-    "Ana",
-    "Johana",
-    "Gordo",
+    "Agus",
     "Chad",
-    "Will"
+    "Will",
+    "Marcos",
+    "Valen"
 ]
 
 # Inicializar algunas variables (aunque no es muy necesario, lo hago para ver que variables voy a usar)
