c873e73d973ef9c436e59168c6eecdcc74ec071c
AguuSz <agus.sepu2000@hotmail.com>
2019-06-13 09:39:00 -0300

    V1.0

--- speechRecognition_mic.py
+++ speechRecognition_mic.py
@@ -1,3 +1,5 @@
+# Archivo encargado del reconocimiento de voz V1.0 || Solo funciona en ingles por el momento
+
 import speech_recognition as sr
 
 r = sr.Recognizer()
@@ -5,8 +7,20 @@ r = sr.Recognizer()
 # Creando la instancia de microfono
 mic = sr.Microphone()
 
-with mic as source:
-    r.adjust_for_ambient_noise(source)
-    audio = r.listen(source)
+while True:
+    with mic as source:
+        # Quitar comentario de la linea de abajo cuando se este trabajando en un ambiente muy ruidoso
+        #r.adjust_for_ambient_noise(source)
+        print("Hable")
+        audio = r.listen(source)
 
-texto = r.recognize_google(audio)
+    try:
+        texto = r.recognize_google(audio)
+        print("Dijiste: " + texto)
+        if texto == "stop":
+            print("Cerrando...")
+            break
+    except sr.UnknownValueError:
+        print("No se entendio")
+    except sr.RequestError:
+        print("No se pudo acceder a una conexion de internet... Verifica tu conexion e intentalo de nuevo!")
